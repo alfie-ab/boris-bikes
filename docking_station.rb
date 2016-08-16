d3113df3b6b14925725c867b56cce67cb7f51bf6
Alfie <alfie_ab@hotmail.co.uk>
2016-08-16 17:28:32 +0100

    can release and dock multiple bikes

--- docking_station.rb
+++ docking_station.rb
@@ -8,26 +8,34 @@ class DockingStation
   attr_reader :docked_bikes
   attr_reader :capacity
 
-  def initialize(capacity = 99)
+  def initialize(capacity = 20)
     @docked_bikes = []
     @capacity = capacity
   end
 
-  def release_bike
-    if @docked_bikes.empty?
-      raise "No bikes in docking station"
-    else
-      returned_bike = @docked_bikes[0]
-      @docked_bikes.delete(@docked_bikes[0])
-      returned_bike
+  def release_bike(num = 1)
+    bikes_to_release = []
+    num.times do
+
+      if @docked_bikes.empty?
+          raise "No bikes in docking station"
+        break
+      else
+        bikes_to_release << @docked_bikes[0]
+        @docked_bikes.delete_at(0)
+      end
     end
+    return bikes_to_release
   end
 
-  def dock_bike(bike)
-    if @docked_bikes.count >= @capacity
-      raise "This docking station is full"
-    else
-      @docked_bikes << bike
+  def dock_bike(*bikes)
+    bikes.each do |bike|
+      if @docked_bikes.count >= @capacity
+        raise "This docking station is full"
+        break
+      else
+        @docked_bikes << bike
+      end
     end
   end
 end
